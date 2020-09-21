5071dca06d46773262fb123a602b343377bfa49f
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-09-21 10:18:04 +0700

    commit destroy
    abc

--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from 'express';
-
-export function routes(app: Express) {
-    app.route("/").get((req, res) => {
-      res.render("index");
-    });
-}
--- tools/copyAssets.ts
+++
@@ -1,6 +0,0 @@
-import * as shell from "shelljs";
-
-// Copy all the view templates
-shell.cp( "-R", "src/views", "dist/" );
-shell.cp( "-R", "src/cert", "dist/" );
-shell.cp( "-R", "src/logs", "dist/src" );
