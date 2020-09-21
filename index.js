6c138cbe9d681581ef8b242ccaeba3b34fcc86ca
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-09-21 10:46:06 +0700

    commit app

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express= require("express");
 
-
+var PORT = process.env.PORT || 8080;
 const app = express();
-app.listen(8080, () => {
-    console.log(`Server is running on port 8080.`);
-  
-  
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
