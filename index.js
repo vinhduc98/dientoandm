dc0593cd757e6cf3aebdb8c308b77fc776ea1222
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-09-21 11:18:54 +0700

    commit typescript
    chuyển từ js sang ts

--- index.js
+++
@@ -1,9 +0,0 @@
-const express= require("express");
-
-var PORT = process.env.PORT || 8080;
-const app = express();
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import http from 'http';
+
+
+var PORT = process.env.PORT || 8080;
+
+const app = express();
+let httpServer = new http.Server(app)
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
