6fa17275de48b58cb0d05e4474b23e2e68fc74c5
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-09-26 11:30:31 +0700

    update swagger
    đổi lại ví dụ

--- src/swagger/file.swagger.ts
+++ src/swagger/file.swagger.ts
@@ -32,7 +32,7 @@ export const uploadImage:any={
                 example: {
                     status: 1,
                     description: "Ok",
-                    data:['upload_a4755d91fa72bc6440527292f126fdc2.png']
+                    data:['https://res.cloudinary.com/adsun/image/upload/v1601094582/yfjbzf4d6nruxgsabsxu.jpg']
                 }
             }
             }
