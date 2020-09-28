e37c62b7d0789cda51baa2a37d6301b37bdb81ad
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-09-28 09:54:22 +0700

    commit
    update lại db

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,21 +1,8 @@
-// export const dbcookingrecipe = {
-//     HOST: "bjsjmon4rexifmloqzmn-mysql.services.clever-cloud.com",
-//     USER: "ul18hbpmbxdpyklj",
-//     PASSWORD: "4Qz8mZj49zlVVGkZW7q6",
-//     DB:"bjsjmon4rexifmloqzmn", // cookingrecipe
-//     pool: {
-//         max: 25,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000,
-//     },
-// }
-
 export const dbcookingrecipe = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "12345",
-    DB:"cookingrecipe", // cookingrecipe
+    HOST: "bjsjmon4rexifmloqzmn-mysql.services.clever-cloud.com",
+    USER: "ul18hbpmbxdpyklj",
+    PASSWORD: "4Qz8mZj49zlVVGkZW7q6",
+    DB:"bjsjmon4rexifmloqzmn", // cookingrecipe
     pool: {
         max: 25,
         min: 0,
@@ -23,3 +10,16 @@ export const dbcookingrecipe = {
         idle: 10000,
     },
 }
+
+// export const dbcookingrecipe = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "12345",
+//     DB:"cookingrecipe", // cookingrecipe
+//     pool: {
+//         max: 25,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// }
