88703705f4f8b2efc6e8244e3401bd59ff27d413
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-09-28 10:14:13 +0700

    up lại db

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,15 +1,15 @@
-export const dbcookingrecipe = {
-    HOST: "bjsjmon4rexifmloqzmn-mysql.services.clever-cloud.com",
-    USER: "ul18hbpmbxdpyklj",
-    PASSWORD: "4Qz8mZj49zlVVGkZW7q6",
-    DB:"bjsjmon4rexifmloqzmn", // cookingrecipe
-    pool: {
-        max: 25,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-}
+// export const dbcookingrecipe = {
+//     HOST: "bjsjmon4rexifmloqzmn-mysql.services.clever-cloud.com",
+//     USER: "ul18hbpmbxdpyklj",
+//     PASSWORD: "4Qz8mZj49zlVVGkZW7q6",
+//     DB:"bjsjmon4rexifmloqzmn", // cookingrecipe
+//     pool: {
+//         max: 25,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// }
 
 // export const dbcookingrecipe = {
 //     HOST: "localhost",
@@ -23,3 +23,16 @@ export const dbcookingrecipe = {
 //         idle: 10000,
 //     },
 // }
+
+export const dbcookingrecipe = {
+    HOST: "bsxxfszfvzpmvubkd1ir-mysql.services.clever-cloud.com",
+    USER: "umelrn6wka9ixhml",
+    PASSWORD: "eKguHqjtEYoCSaDIbgao",
+    DB:"bsxxfszfvzpmvubkd1ir", // cookingrecipe
+    pool: {
+        max: 25,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+}
