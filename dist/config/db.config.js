d3be197e56f1eeb11617d26a79a3bc3db238127f
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2020-10-13 14:25:43 +0700

    update
    update tổng

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -13,18 +13,18 @@
 // }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbcookingrecipe = void 0;
-exports.dbcookingrecipe = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "12345",
-    DB: "cookingrecipe",
-    pool: {
-        max: 25,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-};
+// export const dbcookingrecipe = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "12345",
+//     DB:"cookingrecipe", // cookingrecipe
+//     pool: {
+//         max: 25,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// }
 // export const dbcookingrecipe = {
 //     HOST: "bsxxfszfvzpmvubkd1ir-mysql.services.clever-cloud.com",
 //     USER: "umelrn6wka9ixhml",
@@ -37,4 +37,16 @@ exports.dbcookingrecipe = {
 //         idle: 10000,
 //     },
 // }
+exports.dbcookingrecipe = {
+    HOST: "103.69.193.235",
+    USER: "cloudcomputing",
+    PASSWORD: "123456",
+    DB: "cookingrecipe",
+    pool: {
+        max: 25,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
 //# sourceMappingURL=db.config.js.map
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -24,11 +24,25 @@
 //     },
 // }
 
+// export const dbcookingrecipe = {
+//     HOST: "bsxxfszfvzpmvubkd1ir-mysql.services.clever-cloud.com",
+//     USER: "umelrn6wka9ixhml",
+//     PASSWORD: "eKguHqjtEYoCSaDIbgao",
+//     DB:"bsxxfszfvzpmvubkd1ir", // cookingrecipe
+//     pool: {
+//         max: 25,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// }
+
+
 export const dbcookingrecipe = {
-    HOST: "bsxxfszfvzpmvubkd1ir-mysql.services.clever-cloud.com",
-    USER: "umelrn6wka9ixhml",
-    PASSWORD: "eKguHqjtEYoCSaDIbgao",
-    DB:"bsxxfszfvzpmvubkd1ir", // cookingrecipe
+    HOST: "103.69.193.235",
+    USER: "cloudcomputing",
+    PASSWORD: "123456",
+    DB:"cookingrecipe", // cookingrecipe
     pool: {
         max: 25,
         min: 0,
