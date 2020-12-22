92d318cbde6c0d8aeeaa6ac83c7bb6798889ce20
Duc <vinhduc4498@gmail.com>
2020-12-22 08:26:31 +0700

    đổi hình
    đổi hình

--- dist/config/defaultimg.config.js
+++ dist/config/defaultimg.config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.img = void 0;
 exports.img = {
-    iconlogin: '1604395075733.png'
+    iconlogin: '1608599868227.jpg'
 };
 //# sourceMappingURL=defaultimg.config.js.map
